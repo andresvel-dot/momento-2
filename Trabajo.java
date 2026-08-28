65c2dcc42d0efe2b1d8f4020127263b742ae11da
Andres Velez <andresvel@lajoseroberto.edu.co>
2026-08-27 20:39:19 -0500

    2 Avance del trabajo del momento 2

--- Trabajo.java
+++ Trabajo.java
@@ -13,6 +13,43 @@ public class Trabajo {
       double IMPUESTO_IVA = 0.19;
       double DESCUENTO_PRODUCTO= 0.10;
       double precioTotal;
+
+         do   {
+            System.out.println("CAJA REGISTRADORA");
+            System.out.println("¿Que producto desea comprar?");
+            System.out.println("NOTA: LOS PRODUCTOS ESTAN  ENUMERADOS, RECUERDA SEGUIR CON EL ORDEN EN EL QUE ESTA ENUMERADA CADA OPCION DE SELECCION Y DEFINALIZAR COMPRA");
+            System.out.println("opcion 1. Producto basico: 20.000 ");
+            System.out.println("opcion 2.Producto medio: 70.000 ");
+             System.out.println("opcion 3. premium: 150.000");
+            System.out.println("opcion 4. Desea finalizar compra");
+            opcion=andres.nextInt();
+            
+            switch (opcion)  {
+
+
+                  case 1: System.out.println("El producto vale: 20.000");
+                      TOTAL_BASICO += 20000;
+                    break;
+                     
+                case 2: System.out.println("El producto vale: 70.000");
+                     TOTAL_MEDIO += 70000;
+                     break;
+
+                        
+                     
+                case 3: System.out.println("El producto vale: 150.000");
+                     totalPremium += 150000;
+                       break;
+
+                     
+                case 4: System.out.println("FINALIZAR COMPRA");
+                    break;
+                   
+                    default:
+                        System.out.println("Debe de ingresar el numero correcto para escoger el producto");
+                    break;
+                } 
+
       
  
         
