75ddce01ca55db2af5a78d96605b8e9ae6ea1caa
Andres Velez <andresvel@lajoseroberto.edu.co>
2026-08-27 20:21:37 -0500

    1 Avance en el entregable del momento 2

---
+++ Trabajo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Trabajo {
+    public static void main(String[] args) {
+
+        Scanner andres= new Scanner (System.in);
+
+         int opcion;
+      double TOTAL_BASICO=0;
+      double TOTAL_MEDIO=0;
+      double totalPremium=0;
+      double precioFinal;
+      double IMPUESTO_IVA = 0.19;
+      double DESCUENTO_PRODUCTO= 0.10;
+      double precioTotal;
+      
+ 
+        
+
+ andres.close();
+     
+    }
+}
